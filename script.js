4b3c79b36d5d64fa545fe43b7aff4e17338b1cb1
Ansari-Mohd-Saad <157679873+MohdSaad7766@users.noreply.github.com>
2025-06-24 11:52:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function firstWord(s) {
-  return s.split(" ").[0];
+  return s.split(" ")[0];
 }
 
 // Do not change the code below
 
-// const s = prompt("Enter String:");
-// alert(firstWord(s));
+const s = prompt("Enter String:");
+alert(firstWord(s));
